99c36acf09559da37e6380fc63bc9af5486a66e5
mygeorge <mygeorgeline@163.com>
2020-09-18 22:01:10 +0800

    vue 提交的知识点

--- vue-l/src/App.vue
+++ vue-l/src/App.vue
@@ -7,11 +7,37 @@
     <!--  加冒号表示变量,不加冒号 -->
     <!-- <mycomponent  myName="name" age="20"/> -->
     <!--  事件对应一个处理函数 -->
-    <Helloview title="hello" @onEvent="getsendHendler"/>
+    <!-- <Helloview title="hello" @onEvent="getsendHendler"/> -->
     <div>
-      {{current}} 
+      <!-- {{current}}  -->
       <!--  通过 data 可以拿到 子传父的数据 -->
     </div>
+    <!-- <div>
+      {{Compsolt}}
+    </div> -->
+    <Compsolt>
+      <template #header>
+        <p>
+          我是头部
+        </p>
+      </template>
+      <h3>我是标题</h3>
+      <h4>我是副标题</h4>
+      <p>
+        {{msg}}
+      </p>
+      <!--  定义name footer -->
+      <template v-solt:footer>
+        <p>我是居中显示的</p>
+      </template>  
+      <!--  通过名称拿到数据    数据插槽-->
+      <!-- <template v-slot:dataSlot="{ getDate }">
+        <h1>{{ getDate }}</h1>
+      </template> -->
+      <template v-slot:dataSlot="slotPress">
+       <h1> {{slotPress.getDate}}</h1>
+        </template>
+    </Compsolt>
     <!-- Invalid prop: type check failed for prop "age". Expected Number with value NaN, got String with value "age".
     无效属性：属性“age”的类型检查失败。需要值为NaN的数字，而得到的字符串值为“age”。
  -->
@@ -23,6 +49,7 @@ import kindonce from './components/kindonce'
 import styleclass from './components/styleclass'
 import mycomponent from './components/mycomponents'
 import Helloview from './components/Helloview'
+import Compsolt from './components/Compsolt'
 export default {
   name: 'App',
   //  注入组件,挂载到...
@@ -30,7 +57,8 @@ export default {
     return{
       age:20,
       arrays:['xiaomao','xiaogou','xiaohua'],
-      current:''//父传子
+      current:'',//父传子
+      msg:'我是谁',
     }
   },
   methods:{
@@ -45,6 +73,7 @@ export default {
     'style-view':styleclass,
      mycomponent,
      Helloview,
+     Compsolt,
   }
 }
 </script>
---
+++ vue-l/src/components/compsolt.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <slot name="header"></slot>
+    <slot></slot>
+    <p>
+    润扬品牌网站
+    </p>
+    <!-- name 插槽 使用-->
+    <slot name="footer"></slot>
+    <slot :getDate="getDate" name="dataSlot"></slot> 
+    <!--  把数据传递给 父组件  数据插槽-->
+  </div>
+</template>
+
+<script>
+export default {
+    name:"compsolt",
+    data(){
+        return{
+            // msg:'我是谁'
+            //  作用域插槽
+            getDate:'我是有准备的数据'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
